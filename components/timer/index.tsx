ff0ae8ac6bea4fa4d486fe73db2db8f4330eeb06
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-16 14:53:44 -0500

    timer displays tomato when not running, in correct format

--- components/timer/index.tsx
+++ components/timer/index.tsx
@@ -5,18 +5,21 @@ import ChangeTimes from '../changeTimes'
 
   
   const Timer: React.FC = (props) => {
-    const [tomato, SetTomato] = useState(0.5);
+    const [tomato, SetTomato] = useState(30);
     const [timeLeft, setTimeLeft] = useState(tomato);
     const [startTime, setStartTime] = useState(0);
     const [endTime, setEndTime] = useState(0);
+    const [timerIsRunning, setTimerIsRunning] = useState(false);
 
     function runTimer(){
         let finalTime = new Date(Date.now() + tomato * 60 * 1000).getTime();
         setStartTime(Date.now());
         setEndTime(finalTime);
       const tick =  setInterval(()=>{
+          setTimerIsRunning(true);
             if(Date.now() >= finalTime){
                 setTimeLeft(tomato);
+                setTimerIsRunning(false);
                clearInterval(tick);
             } else {
                 setTimeLeft((finalTime - Date.now()))
@@ -24,20 +27,22 @@ import ChangeTimes from '../changeTimes'
         }, 1000);
     }
 
-    function calcTimeLeft(){
-        let minutes = Math.floor(timeLeft / (1000 * 60))
+    function calcTimeLeft(timeSpan: number){
+        let minutes = Math.floor(timeSpan / (1000 * 60))
         let minutesString = `${minutes}`;
         if (minutes < 10) minutesString = `0${minutesString}`;
-        let seconds = Math.floor(timeLeft / 1000);
+        let seconds = Math.floor((timeSpan - minutes *60 * 1000) / 1000);
         let secondsString = `${seconds}`;
         if (seconds < 10) secondsString = `0${secondsString}`
+        // console.log(minutes);
+        // console.log(seconds);
 
         return `${minutesString} : ${secondsString}`
     }
     return (
         <View>
             <Text>Hello!</Text>
-            <Text>{calcTimeLeft()}</Text>
+            <Text>{timerIsRunning ? calcTimeLeft(timeLeft) : calcTimeLeft((tomato * 60 * 1000))}</Text>
             <Button
                 onPress={runTimer}
                 title="Start"
