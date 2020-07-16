6e130fb0b1dd57f58ecf3ff658ce260e7c17b8a4
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-16 14:36:02 -0500

    time left interval is now being formatted properly

--- components/timer/index.tsx
+++ components/timer/index.tsx
@@ -5,7 +5,7 @@ import ChangeTimes from '../changeTimes'
 
   
   const Timer: React.FC = (props) => {
-    const [tomato, SetTomato] = useState(1);
+    const [tomato, SetTomato] = useState(0.5);
     const [timeLeft, setTimeLeft] = useState(tomato);
     const [startTime, setStartTime] = useState(0);
     const [endTime, setEndTime] = useState(0);
@@ -19,15 +19,25 @@ import ChangeTimes from '../changeTimes'
                 setTimeLeft(tomato);
                clearInterval(tick);
             } else {
-                setTimeLeft((finalTime - Date.now()) / 60000)
+                setTimeLeft((finalTime - Date.now()))
             }
         }, 1000);
     }
 
+    function calcTimeLeft(){
+        let minutes = Math.floor(timeLeft / (1000 * 60))
+        let minutesString = `${minutes}`;
+        if (minutes < 10) minutesString = `0${minutesString}`;
+        let seconds = Math.floor(timeLeft / 1000);
+        let secondsString = `${seconds}`;
+        if (seconds < 10) secondsString = `0${secondsString}`
+
+        return `${minutesString} : ${secondsString}`
+    }
     return (
         <View>
             <Text>Hello!</Text>
-            <Text>{timeLeft.toFixed(2)}</Text>
+            <Text>{calcTimeLeft()}</Text>
             <Button
                 onPress={runTimer}
                 title="Start"
